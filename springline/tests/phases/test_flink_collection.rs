b8dd10ba24623fc6a0ead02737c43d8ce0b42e68
dmrolfs <WV#^MguUW&ar4tc8@UUv&*MxY%bFWf7nvfh>
2021-12-06 18:58:21 -0800

    test taskmanagers collect

--- springline/tests/phases/test_flink_collection.rs
+++ springline/tests/phases/test_flink_collection.rs
@@ -254,3 +254,73 @@ async fn test_flink_retry_jobs_collect() {
     assert_eq!(status, 404);
 }
 
+#[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+async fn test_flink_simple_taskmanagers_collect() {
+    once_cell::sync::Lazy::force(&proctor::tracing::TEST_TRACING);
+    let main_span = tracing::info_span!("test_flink_simple_taskmanagers_collect");
+    let _ = main_span.enter();
+
+    let mock_server = MockServer::start().await;
+
+    let cpu_load: f64 = 16. * ((0..100_000_000).fake::<i64>() as f64 / 100_000_000.);
+    let heap_used: f64 = 1_000_000_000. * ((0..100_000_000).fake::<i64>() as f64 / 100_000_000.);
+    let heap_committed: f64 = 1_000_000_000. * ((0..100_000_000).fake::<i64>() as f64 / 100_000_000.);
+    let nr_threads: i32 = (1..150).fake();
+
+    let b = json!([
+        { "id": "Status.JVM.CPU.Load", "max": cpu_load, },
+        { "id": "Status.JVM.Memory.Heap.Used", "max": heap_used, },
+        { "id": "Status.JVM.Memory.Heap.Committed", "max": heap_committed, },
+        { "id": "Status.JVM.Threads.Count", "max": nr_threads as f64, },
+    ]);
+
+    let metric_response = ResponseTemplate::new(200).set_body_json(b);
+
+    Mock::given(method("GET"))
+        .and(path("/taskmanagers/metrics"))
+        .respond_with(metric_response)
+        .expect(2)
+        .mount(&mock_server)
+        .await;
+
+    let orders = &springline::phases::collection::flink_metrics::STD_METRIC_ORDERS;
+    let client = assert_ok!(reqwest::Client::builder().default_headers(HeaderMap::default()).build());
+    let retry_policy = ExponentialBackoff::builder().build_with_max_retries(3);
+    let client = ClientBuilder::new(client)
+        .with(RetryTransientMiddleware::new_with_policy(retry_policy))
+        .build();
+    let url = format!("{}/", &mock_server.uri());
+    let context = TaskContext { client, base_url: assert_ok!(Url::parse(url.as_str())), };
+
+    let gen = assert_some!(assert_ok!(springline::phases::collection::flink_metrics::make_root_scope_collection_task(
+        FlinkScope::TaskManagers,
+        &orders,
+        context
+    )));
+
+    let actual: Telemetry = assert_ok!(gen().await);
+    assert_eq!(
+        actual,
+        maplit::hashmap! {
+            "cluster.task_cpu_load".to_string() => cpu_load.into(),
+            "cluster.task_heap_memory_used".to_string() => heap_used.into(),
+            "cluster.task_heap_memory_committed".to_string() => heap_committed.into(),
+            "cluster.task_nr_threads".to_string() => nr_threads.into(),
+        }.into()
+    );
+
+    let actual: Telemetry = assert_ok!(gen().await);
+    assert_eq!(
+        actual,
+        maplit::hashmap! {
+            "cluster.task_cpu_load".to_string() => cpu_load.into(),
+            "cluster.task_heap_memory_used".to_string() => heap_used.into(),
+            "cluster.task_heap_memory_committed".to_string() => heap_committed.into(),
+            "cluster.task_nr_threads".to_string() => nr_threads.into(),
+        }.into()
+    );
+
+    let status = assert_ok!(reqwest::get(format!("{}/missing", &mock_server.uri())).await).status();
+    assert_eq!(status, 404);
+}
+
