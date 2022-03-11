d1e2da8812f8747f927291e11ae738741f375875
dmrolfs <WV#^MguUW&ar4tc8@UUv&*MxY%bFWf7nvfh>
2022-03-11 08:42:28 -0800

    add arguments to local docker run script

--- scripts/run_springline_local.sh
+++ scripts/run_springline_local.sh
@@ -2,13 +2,53 @@
 set -x
 set -eo pipefail
 
-PROJECT_HOME="${SPRINGLINE_HOME:=./springline}"
+if [ -z "${KUBECONFIG}" ]; then
+  echo "Error: KUBECONFIG environment variable is not set" >&2
+  exit 1
+fi
 
+if [ -z "$HA_CREDENTIALS" ]; then
+  echo "Error: HA_CREDENTIALS environment variable is not set" >&2
+  exit 1
+fi
+
+MEM_OPT="64m"
+RM_OPT=""
+
+PARAMS=""
+while (( "$#" )); do
+  case "$1" in
+    -m|--memory)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        MEM_OPT="$2"
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    --rm)
+      RM_OPT="--rm"
+      PARAMS="$2"
+      shift 1
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+
+PROJECT_HOME="${SPRINGLINE_HOME:=./springline}"
 RESOURCES="$(pwd)/resources"
 
 docker run -d -it \
-  -m 64m \
-  --rm \
+  -m ${MEM_OPT} \
+  ${RM_OPT} \
   --name springline \
   --mount type=bind,source="${RESOURCES}",target="/app/resources" \
   --mount type=bind,source="${KUBECONFIG}",target="/secrets/environment.kubeconfig" \
