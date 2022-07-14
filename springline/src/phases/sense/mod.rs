cf004ee70c17e886f331b8507e4b8912c655e89c
dmrolfs <WV#^MguUW&ar4tc8@UUv&*MxY%bFWf7nvfh>
2022-07-14 15:46:58 -0700

    set supplemental metrics

--- springline/src/phases/sense/mod.rs
+++ springline/src/phases/sense/mod.rs
@@ -19,6 +19,8 @@ pub async fn make_sense_phase(
     name: &str, context: FlinkContext, settings: &SensorSettings,
     auxiliary_sensors: Vec<Box<dyn SourceStage<Telemetry>>>, machine_node: MachineNode,
 ) -> Result<(SenseBuilder<MetricCatalog>, TickApi)> {
+    do_set_metric_catalog_supplemental_telemetry(settings);
+
     let mut sensors = do_make_modular_sensors(&settings.sensors, auxiliary_sensors).await?;
 
     let scheduler = Tick::new(
@@ -43,6 +45,31 @@ pub async fn make_sense_phase(
         Sense::builder(name, sensors, &settings.clearinghouse, machine_node),
         tx_scheduler_api,
     ))
+}
+
+#[tracing::instrument(level = "trace", skip(settings))]
+fn do_set_metric_catalog_supplemental_telemetry(settings: &SensorSettings) {
+    let assigned_telemetry: HashSet<&str> = flink::STD_METRIC_ORDERS.iter().map(|o| o.telemetry().1).collect();
+    let ordered_telemetry: HashSet<&str> = settings.flink.metric_orders.iter().map(|o| o.telemetry().1).collect();
+    let unassigned_telemetry: HashSet<String> = ordered_telemetry
+        .difference(&assigned_telemetry)
+        .map(|t| t.to_string())
+        .collect();
+
+    match crate::flink::SUPPLEMENTAL_TELEMETRY.set(unassigned_telemetry) {
+        Ok(()) => {
+            tracing::info!(
+                supplemental_telemetry=?crate::flink::SUPPLEMENTAL_TELEMETRY,
+                "adding supplemental metrics to the MetricCatalog's subscription. When (optionally) populated, they can be found in the MetricCatalog.custom field."
+            )
+        },
+        Err(unassigned_telemetry) => {
+            tracing::error!(
+                ?unassigned_telemetry,
+                "Could not set unassigned metric orders into MetricCatalog's supplemental telemetry subscription."
+            );
+        },
+    };
 }
 
 /// Makes `SourceStage`-based sensors specified in the sensor::sensors.
