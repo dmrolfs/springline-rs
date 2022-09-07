f96bedda6179310b405ad37f65fdb1edd48603f5
dmrolfs <WV#^MguUW&ar4tc8@UUv&*MxY%bFWf7nvfh>
2022-09-06 22:39:26 -0700

    pretty action outcome display for session log

--- springline/src/phases/act/action.rs
+++ springline/src/phases/act/action.rs
@@ -89,7 +89,7 @@ impl Display for ActionOutcome {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "ActionOutcome {{ {}:{} {:?} }}",
+            "{}:{{ {} @ {:?} }}",
             self.label, self.status, self.duration
         )
     }
